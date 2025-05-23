8a9ee76c34177ba39b3d584f910e5db98a33d7d6
upendrakhadka <khadka.upendra02@gmail.com>
2025-05-23 18:12:38 +0545

    adding backend on git

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import adminRouter from "./routes/adminRoute.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// ✅ Configure CORS for your deployed frontend
+app.use(cors({
+  origin: ['https://medi-link-admin01.vercel.app/'],
+  credentials: true
+}));
+
+// middleware
+app.use(express.json());
+
+// API endpoints
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
